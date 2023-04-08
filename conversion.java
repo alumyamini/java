432b06288c4ce944b74829b6b122c488ae33ef22
alumyamini <113881028+alumyamini@users.noreply.github.com>
2023-04-07 20:38:11 -0700

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,11 @@
+class conversion {
+  public static void main(String[] args) {
+    
+    // binary number
+    String binary = "01011011";
+    
+    // convert to decimal
+    int decimal = Integer.parseInt(binary, 2);
+    System.out.println(binary + " in binary = " + decimal + " in decimal.");
+  }
+}
